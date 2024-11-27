9cd79d663028bb8935ca2bedf096a099b1a10451
Jay Shyam Patel <jayshyampatel133@gmail.com>
2024-11-28 01:09:40 +0545

    chore: Add event dispatching for trip acceptance, start, end, and location updates

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TripAccepted;
+use App\Events\TripEnded;
+use App\Events\TripLocationUpdated;
+use App\Events\TripStarted;
 use App\Models\Trip;
 use Illuminate\Http\Request;
 
@@ -48,6 +52,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripAccepted::dispatch($trip, $request->user());
+
         return $trip;
 
     }
@@ -59,6 +65,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripStarted::dispatch($trip, $request->user());
+
         return $trip;
 
     }
@@ -69,6 +77,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripEnded::dispatch($trip, $request->user());
 
         return $trip;
 
@@ -84,6 +94,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripLocationUpdated::dispatch($trip, $request->user());
 
         return $trip;
 
